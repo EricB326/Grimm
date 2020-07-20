d29b0e191a83e2c4bd33d3825402a8567d03887d
Eric <ericbrkic326@gmail.com>
2020-07-20 14:37:07 +1000

    Begun local stats for entities

---
+++ GrimmGame/Assets/Prototyping/Objects/GlobalScripts/EntityStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityStats : MonoBehaviour
+{
+    [SerializeField] private bool entityHasHitPoints;
+    [SerializeField] private bool entityHasStamina;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GrimmGame/Assets/Prototyping/Objects/GlobalScripts/StaminaSystem.cs
@@ -0,0 +1,14 @@
+﻿/*********************************
+ Author: Eric Brkic
+ Last Modified: 20/07/2020
+ Purpose: Modular system used to drain stamina from entities when called.
+ ********************************/
+
+ /* Engine headers
+  */
+using UnityEngine;
+
+public class Stamina : MonoBehaviour
+{
+
+}
