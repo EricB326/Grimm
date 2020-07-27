788de9198c63825809c5104d68fb52ed1ba93016
Anton <huber.anton@hotmail.com>
2020-07-28 09:46:55 +1000

    Lock on added
    - Character locks on to target with tab.
    - Player will require a target for it to work.
    - Changed lock on cam to function similarly to free cam.
    - Need to add damping to cam still.
    - Need to add custom blends between cameras.

--- GrimmGame/Assets/Prototyping/AntonsPartyPalace/Player/Scripts/PlayerMovementVariables.cs
+++ GrimmGame/Assets/Prototyping/AntonsPartyPalace/Player/Scripts/PlayerMovementVariables.cs
@@ -11,11 +11,12 @@ public class PlayerMovementVariables : MonoBehaviour
 {
     // Multiplied by direction stick input. -1 to 1 on both axis
     public float m_walkSpeed;
+    // Speed will increase to this value.
     public float m_runSpeed;
 
     [Range(0,1)]
     public float m_rotationTime;
-    // Used in the player walk for the time being.
+    // Used in the player walk till access to entity list.
     public GameObject m_target;
 
 
