c67f0fd9caf13e3e8aaabb6993ad85bbb2126c75
Anton <huber.anton@hotmail.com>
2020-09-14 19:16:40 +1000

    Problems with Erics merge.
    - Took out slop rotation.
    -  An endless fucking problem

--- GrimmGame/Assets/Prototyping/AntonsPartyPalace/Player/RootMotionMovementAdjustment.cs
+++ GrimmGame/Assets/Prototyping/AntonsPartyPalace/Player/RootMotionMovementAdjustment.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class RootMotionMovementAdjustment : StateMachineBehaviour
 {
-
-    float lastY = 0;
-
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
@@ -28,72 +25,14 @@ public class RootMotionMovementAdjustment : StateMachineBehaviour
     // OnStateMove is called right after Animator.OnAnimatorMove()
     override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        // Position to move to
-        Vector3 posToMove = animator.deltaPosition;
-        float forwardOffset = 0.5f;
-        float length = 0.1f;
-        Vector3 upwardOffest = new Vector3(0, 1, 0);
-        Vector3 stepUpHeight = new Vector3(0, 0, 0);
-        Vector3 straightDownOffset = new Vector3(0, 0.5f, 0);
-        // Move position up or down on y axis depending on angle below.
-        // Direct Down
-        Ray directDown = new Ray(animator.gameObject.transform.position + straightDownOffset, -Vector3.up);
-       
-        RaycastHit directDownhit;
-        // In Front
-        Ray stepInFrontDown = new Ray(animator.gameObject.transform.position +
-            animator.gameObject.transform.forward * forwardOffset + stepUpHeight + upwardOffest, -Vector3.up);
-        RaycastHit stepInFrontHit;
-       
-
-        // Debug rays
-        Debug.DrawRay(animator.gameObject.transform.position + straightDownOffset, -Vector3.up * 
-            (length + stepUpHeight.y + straightDownOffset.y));
-        Debug.DrawRay(animator.gameObject.transform.position + 
-            animator.gameObject.transform.forward * forwardOffset + 
-            stepUpHeight + upwardOffest, -Vector3.up * (length + stepUpHeight.y + upwardOffest.y));
-
-
-        //if (Physics.Raycast(stepInFrontDown, out stepInFrontHit, length + stepUpHeight.y + upwardOffest.y))
-        //{
-        //    Vector3 direction1 = stepInFrontHit.point - animator.gameObject.transform.position;
-        //    if (direction1.y != 0)
-        //    {
-        //        ////Debug.Log(direction.normalized);
-        //        //float angle = (180 / 3.14159f) * Mathf.Atan(direction.y);
-        //        //// Almost right... maybe?
-        //        //// Need to get an angle
-        //        //posToMove = Quaternion.Euler(angle, 0, 0) * posToMove;
-        //        if (Physics.Raycast(directDown, out directDownhit, length + straightDownOffset.y))
-        //        {
-        //            Vector3 direction2 = directDownhit.point - animator.gameObject.transform.position;
-        //            if (direction2.y != 0)
-        //            {
-        //                Vector3 norm = direction1 - direction2;
-        //                //Debug.Log(direction.normalized);
-        //                float angle = Mathf.Acos(norm.y);
-        //                // Almost right... maybe?
-        //                // Need to get an angle
-        //                posToMove = Quaternion.Euler(angle, 0, 0) * posToMove;
-        //            }
-        //        }
-        //    }
-        //}
-        //else if (Physics.Raycast(directDown, out directDownhit, length + straightDownOffset.y))
-        //{
-        //    Debug.Log("hit");
-        //    float angle = Vector3.Angle(Vector3.up, directDownhit.normal);
-        //    posToMove = Quaternion.Euler(angle, 0, 0) * posToMove;
-        //}
-
-        if (!Physics.Raycast(directDown, out directDownhit, length + straightDownOffset.y))
-        {
-            // Assumed not grounded
-            posToMove.y = -0.2f;
-        }
-
-        // Neeed to remove gravity for this to work.        // Just a translate in the end
-        animator.gameObject.transform.position = animator.gameObject.transform.position + posToMove;
+        SloopHandler mine = animator.gameObject.GetComponent<SloopHandler>();
+        mine.CalculateForward();
+        mine.CalculateForward();
+        mine.CalculateGroundAngle();
+        mine.CheckGround();
+        mine.ApplyGravity();
+        mine.DrawDebugLines();
+        animator.ApplyBuiltinRootMotion();
     }
 
     // OnStateIK is called right after Animator.OnAnimatorIK()
---
+++ GrimmGame/Assets/Prototyping/EricsFun/SloopHandler.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SloopHandler : MonoBehaviour
+{
+    public float height = 1f;
+    public float heightPadding = 0.1f;
+    public float heightCorrectionSpeed = 5f;
+    public LayerMask ground;
+    public float maxGroundAngle = 120f;
+    public bool debugMode;
+
+    float angle;
+    float groundAngle;
+
+    Vector3 forward;
+    RaycastHit hitInfo;
+    bool isGrounded;
+
+    public void CalculateForward()
+    {
+        if (!isGrounded)
+        {
+            forward = transform.forward;
+            return;
+        }
+
+        forward = Vector3.Cross(transform.right, hitInfo.normal);
+    }
+
+    public void CalculateGroundAngle()
+    {
+        if (!isGrounded)
+        {
+            groundAngle = 90f;
+            return;
+        }
+
+        groundAngle = Vector3.Angle(hitInfo.normal, transform.forward);
+    }
+
+    public void CheckGround()
+    {
+        if (Physics.Raycast(transform.position + new Vector3(0, 1, 0), -Vector3.up, out hitInfo, height + heightPadding, ground))
+        {
+            if (Vector3.Distance(transform.position, hitInfo.point) < height)
+            {
+                // transform.position = Vector3.Slerp(transform.position, hitInfo.point, heightCorrectionSpeed * Time.deltaTime);
+                transform.position = hitInfo.point;
+            }
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    public void ApplyGravity()
+    {
+        if (!isGrounded)
+        {
+            transform.position += Physics.gravity * Time.deltaTime;
+        }
+    }
+
+    public void DrawDebugLines()
+    {
+        if (!debugMode)
+            return;
+
+        Debug.DrawLine(transform.position + new Vector3(0, 1, 0), (transform.position + new Vector3(0, 1, 0)) + forward * height * 2, Color.blue);
+        Debug.DrawLine(transform.position + new Vector3(0, 1, 0), (transform.position + new Vector3(0, 1, 0)) - Vector3.up * (height + heightPadding), Color.green);
+    }
+}
+
