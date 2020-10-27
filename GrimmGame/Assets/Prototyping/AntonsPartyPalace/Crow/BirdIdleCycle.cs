65dcacab2f650c1a55b7fb33d13a2188eac5d041
Kaz <kazchesna@gmail.com>
2020-10-27 14:20:40 +1100

    Added Ravens and bones to arena in kazsmap

---
+++ GrimmGame/Assets/Prototyping/AntonsPartyPalace/Crow/BirdIdleCycle.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Quick script to have the birds cycle their idles
+// randomly.
+// Oh god. Unity lies about how to lerp.
+public class BirdIdleCycle : StateMachineBehaviour
+{
+    private float m_timeBetweenNewIdle = 4f;
+    private float m_NextTimeCycle = 0;
+    // Should take 10 frames to reach destination
+    private float m_rate = 0.01f;
+    // How far along the lerp is.
+    // Starts at 0 and increases to 1
+    private float m_distanceSoFar = 1;
+
+    // The blend value at start.
+    private float m_startPosition;
+
+    // The blend value at the end
+    private float m_endPosition;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (m_distanceSoFar >= 1f)
+        {
+            if (m_NextTimeCycle < Time.time)
+            {
+                // Get a new number to move towards.
+                m_endPosition = Random.Range(0, 1f);
+                // Get current position
+                m_startPosition = animator.GetFloat("Blend");
+                // Set how far we've traveled so far.
+                m_distanceSoFar = 0;
+                // Set next time the idle change
+                m_NextTimeCycle = Time.time + m_timeBetweenNewIdle;
+            }
+        }
+        else
+        {
+            m_distanceSoFar += m_rate;
+            animator.SetFloat("Blend", (Mathf.Lerp(m_startPosition, m_endPosition, m_distanceSoFar)));
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
---
+++ GrimmGame/Assets/Prototyping/AntonsPartyPalace/Crow/crowFly.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crowFly : MonoBehaviour
+{
+    private Animator m_animator;
+    private Vector3 m_startPosition;
+    private Collider m_collisionCheck;
+    private bool m_canReturn = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_animator = this.GetComponent<Animator>();
+        // Ranomizes their starting position for then animation.
+        ResetAnimation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the birds come back it will occur in this loop.
+    }
+
+    private void ResetAnimation()
+    {
+        m_animator.Play("Idle_Blend Tree", -1, Random.Range(0, 1f));
+    }
+
+    // Player has spooked them fly away from them.
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            this.transform.LookAt(other.transform);
+
+            
+        }
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            this.transform.LookAt(other.transform);
+
+
+        }
+    }
+
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            // Return to position after x amount of time.
+        }
+    }
+
+
+}
