4ca0f1927918cb9edca1ba4fb12d86b6f4572ec6
Eric <ericbrkic326@gmail.com>
2020-11-09 11:35:05 +1100

    Menu int.

--- GrimmGame/Assets/Prototyping/Jack's Corner/Scripts/MenuControls.cs
+++ GrimmGame/Assets/Prototyping/Jack's Corner/Scripts/MenuControls.cs
@@ -11,11 +11,17 @@ public class MenuControls : MonoBehaviour
     public GameObject[] menuUIs;
     public GameObject menuCollection;
     public GameObject optionCollection;
+    public GameObject controlsCollection;
+    public GameObject creditsCollection;
     public GameObject test;
     public GameObject test2;
+    public GameObject controlsBack;
+    public GameObject creditsBack;
     public float fadeSpeed = 5f;
     public float fadeThreshold = 0.5f; //Otherwise the buttons don't properly fade
     bool optionsFade = true;
+    bool controlsFade = true;
+    bool creditsFade = true;
 
     // Start is called before the first frame update
     void Start()
@@ -46,14 +52,18 @@ public class MenuControls : MonoBehaviour
         Debug.Log("Game Quit");
     }
 
-    public void Credits()
+    public void Controls()
     {
-        //SceneManager.LoadScene("Credits");
-    }
+        StartCoroutine(FadeImageControls(controlsFade));
+	}
 
-    IEnumerator FadeImage(bool fade)
-    {
-        
+	public void Credits()
+	{
+		StartCoroutine(FadeImageCredits(creditsFade));
+	}
+
+	IEnumerator FadeImage(bool fade)
+    { 
         if (fade)
         {
 			foreach (GameObject button in menuUIs)
@@ -120,4 +130,142 @@ public class MenuControls : MonoBehaviour
 			optionsFade = true;
         }
     }
+
+	IEnumerator FadeImageControls(bool fade)
+	{
+		if (fade)
+		{
+			foreach (GameObject button in menuUIs)
+			{
+				button.SetActive(true);
+
+				foreach (Transform child in button.transform)
+				{
+					Image this_image = child.GetComponent<Image>();
+					if (this_image != null && this_image.name == "Highlighted")
+						this_image.gameObject.SetActive(false);
+				}
+			}
+
+			for (int i = 0; i < menuUIs.Length; i++)
+			{
+				Color textMeshColor = menuUIs[i].GetComponentInChildren<TextMeshProUGUI>().color;
+				for (float j = 1; j >= 0 - fadeThreshold; j -= Time.deltaTime * fadeSpeed)
+				{
+					menuUIs[i].GetComponent<Image>().color = new Color(1, 1, 1, j);
+					menuUIs[i].GetComponentInChildren<TextMeshProUGUI>().color = new Color(textMeshColor.r, textMeshColor.g, textMeshColor.b, j);
+
+					yield return null;
+				}
+			}
+
+			menuCollection.SetActive(false);
+			controlsCollection.SetActive(true);
+			EventSystem.current.SetSelectedGameObject(null);
+			EventSystem.current.SetSelectedGameObject(controlsBack);
+			controlsFade = false;
+		}
+		else
+		{
+			menuCollection.SetActive(true);
+			EventSystem.current.SetSelectedGameObject(null);
+			EventSystem.current.SetSelectedGameObject(test2);
+			controlsCollection.SetActive(false);
+
+			for (int i = 0; i < menuUIs.Length; i++)
+			{
+				Color textMeshColor = menuUIs[i].GetComponentInChildren<TextMeshProUGUI>().color;
+				for (float j = 0; j <= 1 + fadeThreshold; j += Time.deltaTime * fadeSpeed)
+				{
+					menuUIs[i].GetComponent<Image>().color = new Color(1, 1, 1, j);
+					menuUIs[i].GetComponentInChildren<TextMeshProUGUI>().color = new Color(textMeshColor.r, textMeshColor.g, textMeshColor.b, j);
+					yield return null;
+				}
+			}
+
+			foreach (Transform child in controlsCollection.transform)
+			{
+				if (child.gameObject.name == "Back Button")
+				{
+					foreach (Transform veryChild in child.transform)
+					{
+						Image this_image = veryChild.GetComponent<Image>();
+						if (this_image != null && this_image.name == "Highlight")
+							this_image.gameObject.SetActive(false);
+					}
+				}
+			}
+
+			controlsFade = true;
+		}
+	}
+
+	IEnumerator FadeImageCredits(bool fade)
+	{
+		if (fade)
+		{
+			foreach (GameObject button in menuUIs)
+			{
+				button.SetActive(true);
+
+				foreach (Transform child in button.transform)
+				{
+					Image this_image = child.GetComponent<Image>();
+					if (this_image != null && this_image.name == "Highlighted")
+						this_image.gameObject.SetActive(false);
+				}
+			}
+
+			for (int i = 0; i < menuUIs.Length; i++)
+			{
+				Color textMeshColor = menuUIs[i].GetComponentInChildren<TextMeshProUGUI>().color;
+				for (float j = 1; j >= 0 - fadeThreshold; j -= Time.deltaTime * fadeSpeed)
+				{
+					menuUIs[i].GetComponent<Image>().color = new Color(1, 1, 1, j);
+					menuUIs[i].GetComponentInChildren<TextMeshProUGUI>().color = new Color(textMeshColor.r, textMeshColor.g, textMeshColor.b, j);
+
+					yield return null;
+				}
+			}
+
+			menuCollection.SetActive(false);
+			creditsCollection.SetActive(true);
+			EventSystem.current.SetSelectedGameObject(null);
+			EventSystem.current.SetSelectedGameObject(creditsBack);
+			creditsFade = false;
+		}
+		else
+		{
+			menuCollection.SetActive(true);
+			EventSystem.current.SetSelectedGameObject(null);
+			EventSystem.current.SetSelectedGameObject(test2);
+			creditsCollection.SetActive(false);
+
+			for (int i = 0; i < menuUIs.Length; i++)
+			{
+				Color textMeshColor = menuUIs[i].GetComponentInChildren<TextMeshProUGUI>().color;
+				for (float j = 0; j <= 1 + fadeThreshold; j += Time.deltaTime * fadeSpeed)
+				{
+					menuUIs[i].GetComponent<Image>().color = new Color(1, 1, 1, j);
+					menuUIs[i].GetComponentInChildren<TextMeshProUGUI>().color = new Color(textMeshColor.r, textMeshColor.g, textMeshColor.b, j);
+					yield return null;
+				}
+			}
+
+			foreach (Transform child in creditsCollection.transform)
+			{
+				if (child.gameObject.name == "Back Button")
+				{
+					foreach (Transform veryChild in child.transform)
+					{
+						Image this_image = veryChild.GetComponent<Image>();
+						if (this_image != null && this_image.name == "Highlight")
+							this_image.gameObject.SetActive(false);
+					}
+				}
+			}
+
+			creditsFade = true;
+		}
+	}
 }
